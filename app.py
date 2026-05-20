c44abe3fe4876c7725aea166075029b705875202
ppa84243-lab <ppa84243@gmail.com>
2026-05-21 07:00:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1067,7 +1067,7 @@ st.markdown(
     """
     <style>
     div.stButton > button {
-        width: 500%;
+        width: 100%;
         min-height: 44px;
         font-size: 18px;
         font-weight: 900;
